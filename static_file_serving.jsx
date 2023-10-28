6a0557a8b013366b64c3dd465dcbd44f03868b75
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-29 02:18:48 +0530

    Static File Serving in NextJS

---
+++ static_file_serving.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+   return (
+      <>
+         <h1>My First Post</h1>
+         <h2>
+            <Link href="/">
+               <a>Home</a>
+            </Link>
+         </h2>
+         <br/">
+         <img src="/sray.png" alt="Home Page Icon" />
+      </>	  
+   )
+}
